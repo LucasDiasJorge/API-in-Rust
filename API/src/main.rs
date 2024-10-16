47633d34455521517f063b7321bcb2990a97690a
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-10-16 20:36:16 -0300

    Serializando e deserializando dados em formato JSON.
    curl -X POST http://localhost:8080/json \
    -H "Content-Type: application/json" \
    -d '{"name": "Lucas", "age": 22}'

--- API/src/main.rs
+++ API/src/main.rs
@@ -1,15 +1,30 @@
 use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+use serde::{Deserialize, Serialize};
 
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello, Actix-Web with Rust!")
+#[derive(Deserialize)]
+struct Info {
+    name: String,
+    age: u8,
+}
+
+#[derive(Serialize)]
+struct Response {
+    message: String,
+}
+
+async fn process_json(info: web::Json<Info>) -> impl Responder {
+    let response_message = format!("Hello, {}! You are {} years old.", info.name, info.age);
+
+    HttpResponse::Ok().json(Response {
+        message: response_message,
+    })
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(hello))
+            .route("/json", web::post().to(process_json))
     })
         .bind("127.0.0.1:8080")?
         .run()
