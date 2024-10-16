d7a8001c9c2afd5d9a3365d0b2b660441dfec25c
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-10-16 20:42:22 -0300

    Aprofundando em tratativas de erros e Responses personalizadas

--- API/src/main.rs
+++ API/src/main.rs
@@ -8,14 +8,37 @@ struct Info {
 }
 
 #[derive(Serialize)]
-struct Response {
+struct SuccessResponse {
+    success: bool,
+    message: String,
+}
+
+#[derive(Serialize)]
+struct ErrorResponse {
+    success: bool,
     message: String,
 }
 
 async fn process_json(info: web::Json<Info>) -> impl Responder {
+
+    if info.age == 0 {
+        return HttpResponse::BadRequest().json(ErrorResponse {
+            success: false,
+            message: "Age must be greater than 0".to_string(),
+        });
+    }
+
+    if info.name.len() < 3 {
+        return HttpResponse::BadRequest().json(ErrorResponse {
+            success: false,
+            message: "Name must be at least 3 characters long".to_string(),
+        });
+    }
+
     let response_message = format!("Hello, {}! You are {} years old.", info.name, info.age);
 
-    HttpResponse::Ok().json(Response {
+    HttpResponse::Ok().json(SuccessResponse {
+        success: true,
         message: response_message,
     })
 }
